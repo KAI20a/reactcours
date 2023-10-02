c4668e8e33211393694cd446f0b0008d454192ad
KAI20a <kai.20@uni-dubna.ru>
2023-10-02 20:41:03 +0300

    push

--- dd1.js
+++ dd1.js
@@ -1,32 +1,65 @@
-function manyChecks() {
-    let a = 10;
-    console.log(`a = ${a}`);
-    
-    return (
-      a > 10 ? 'a is bigger than 10' : 'a is less than or equal to 10 ' + (a === 5 ? 'an example of a special case' : '')) + (a === 15 ? 'but a is not 15' : '')+ (a > 5 ? 'and a is greater than 5' : 'and a is less than or equal to 5 ') + (a % 2 ? ' and a is odd' : ' and a is even ');
-  }
-  manyChecks();
-
-  function first()
+function first()
   {
     if(a > 10)
-        itog += 'a is bigger than 10';
+        itog += 'a is bigger than 10 ';
     else
-        itog += 'a is less than or equal to 10';
+        itog += 'a is less than or equal to 10 ';
     if(a=== 5)
-        itog += 'an example of a special case';
+        itog += 'an example of a special case ';
     if(a === 15)
-        itog+= 'but a is not 15';
+        itog+= 'but a is not 15 ';
     if(a > 5)
-        itog+= 'and a is greater than 5';
+        itog+= 'and a is greater than 5 ';
     else
         itog+='and a is less than or equal to 5 ';
     if(a % 2)
-        itog+= 'and a is odd';
+        itog+= 'and a is odd.';
     else
-        itog+= 'and a is even';
+        itog+= 'and a is even.';
+    return itog;
+  }
+
+  function second(){
+    switch(true)
+    {
+        case(a > 10):
+            itog += 'a is bigger than 10 ';
+            break;
+        default:
+            itog += 'a is less than or equal to 10 ';
+            break;
+    }
+    switch(true)
+    {
+        case(a === 5):
+            itog += 'an example of a special case ';
+            break;
+        case (a === 15):
+            itog += 'but a is not 15 ';
+            break;
+    }
+    switch(true)
+    {
+        case(a > 5):
+            itog+= 'and a is greater than 5 ';
+            break;
+        default:
+            itog+='and a is less than or equal to 5 ';
+            break;
+    }
+    switch(true)
+    {
+        case(a % 2):
+            itog+= 'and a is odd.';
+            break;
+        default:
+            itog+= 'and a is even.';
+            break;
+    }
     return itog;
   }
   let itog = '';
   let a = 10;
-  console.log(first())
+  console.log(first());
+  itog = '';
+  console.log(second());
