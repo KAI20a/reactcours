8b452fe3f63de13ca3f77ed04a083b87c8b5cc08
KAI20a <kai.20@uni-dubna.ru>
2023-10-04 16:25:28 +0300

    push

--- calc.js
+++ calc.js
@@ -1,11 +1,26 @@
 function addInText(text)
 {
-    let block = document.getElementById("inputBlock");
+    let block = document.getElementById('inputBlock');
     block.value += text;
 }
 function Calc()
 {
-    let block = document.getElementById("inputBlock");
+    let block = document.getElementById('inputBlock');
     let a = block.value;
-    alert(a);
+    let b;
+    let newblock = document.getElementById('history')
+    try {
+        b = eval(a);
+        block.value = b;
+        let p = document.createElement('p');
+        p.textContent = a + "=" + b;
+        newblock.appendChild(p)
+    } catch (error) {
+        alert('Ошибка в данных. Вычисление невозможно.')
+    }
+}
+function Clear()
+{
+    let block = document.getElementById('inputBlock');
+    block.value = '';
 }
